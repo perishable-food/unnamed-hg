b0471b62057ad89261f1d00d7531be8c88f46a7f
perishable-food <118243853+perishable-food@users.noreply.github.com>
2024-08-23 01:52:11 -0700

    level cap candy (thanks mixone!)

--- asm/general_hooks.s
+++ asm/general_hooks.s
@@ -393,3 +393,19 @@ mov pc, r1
 
 CanUseItemOnMonInParty_return_address:
 .word 0
+
+.global UseItemOnPokemon_hook
+UseItemOnPokemon_hook:
+ldr r5, =UseItemOnPokemon_return_address
+mov r6, lr
+str r6, [r5]
+pop {r5-r6}
+bl UseItemOnPokemon
+ldr r1, =UseItemOnPokemon_return_address
+ldr r1, [r1]
+mov pc, r1
+
+.pool
+
+UseItemOnPokemon_return_address:
+.word 0
--- src/field/overworld_table.c
+++ src/field/overworld_table.c
@@ -1043,9 +1043,9 @@ struct OVERWORLD_TAG gOWTagToFileNum[] = // skip down a bit to see the parts tha
     { .tag = 1172, .gfx =  918, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_PETILIL
     { .tag = 1173, .gfx =  919, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_LILLIGANT
     { .tag = 1174, .gfx = 1349, .callback_params = OVERWORLD_SIZE_SMALL}, // hisui
-    { .tag = 1175, .gfx =  920, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_BASCULIN
-    { .tag = 1176, .gfx =  921, .callback_params = OVERWORLD_SIZE_SMALL}, // red stripe
-    { .tag = 1177, .gfx = 1350, .callback_params = OVERWORLD_SIZE_SMALL}, // blue stripe
+    { .tag = 1175, .gfx =  921, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_BASCULIN
+    { .tag = 1176, .gfx = 1350, .callback_params = OVERWORLD_SIZE_SMALL}, // blue stripe
+    { .tag = 1177, .gfx =  920, .callback_params = OVERWORLD_SIZE_SMALL}, // white stripe
     { .tag = 1178, .gfx =  922, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_SANDILE
     { .tag = 1179, .gfx =  923, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_KROKOROK
     { .tag = 1180, .gfx =  924, .callback_params = OVERWORLD_SIZE_SMALL}, // SPECIES_KROOKODILE
