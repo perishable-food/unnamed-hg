19dcdeec58e92db0733f6af9dbcbe356b77de3d8
perishable-food <118243853+perishable-food@users.noreply.github.com>
2024-09-27 23:16:05 -0700

    remove new world activation

--- src/battle/battle_script_commands.c
+++ src/battle/battle_script_commands.c
@@ -2405,9 +2405,6 @@ BOOL btl_scr_cmd_EC_updateterrainoverlay(void *bw UNUSED, struct BattleStruct *s
         case MOVE_PSYCHIC_TERRAIN:
             sp->terrainOverlay.type = PSYCHIC_TERRAIN;
             break;
-		case MOVE_ELECTRIFY:
-			sp->terrainOverlay.type = NEW_WORLD;
-			break;
         default:
             // I think this could work for moves that remove terrain
             sp->terrainOverlay.type = TERRAIN_NONE;
