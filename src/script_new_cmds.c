3b82ef670d4ff79939392de9c5984e6fc130c076
perishable-food <Bruhm2891@gmail.com>
2025-09-02 18:14:20 -0700

    iv maxer outer script command (have to check ingame still)

--- src/script_new_cmds.c
+++ src/script_new_cmds.c
@@ -3,7 +3,8 @@
 #include "../include/repel.h"
 #include "../include/constants/file.h"
 
-#define SCRIPT_NEW_CMD_REPEL_USE    0
+#define SCRIPT_NEW_CMD_REPEL_USE      0
+#define SCRIPT_NEW_CMD_BOTTLE_CAP_USE 1
 
 #define SCRIPT_NEW_CMD_MAX          256
 
@@ -21,6 +22,35 @@ BOOL Script_RunNewCmd(SCRIPTCONTEXT *ctx) {
             break;
 
         default: break;
+
+    case SCRIPT_NEW_CMD_BOTTLE_CAP_USE:;
+#ifdef BOTTLE_CAP_MAX_IVS
+            FieldSystem *fsys = ctx->fsys;
+            struct PartyPokemon *pp;
+            struct Party *party = SaveData_GetPlayerPartyPtr(fsys->savedata);
+            u8 iv = 31;
+            u8 pos = GetScriptVar(0x8008);
+            pp = Party_GetMonByIndex (party, pos);
+            switch (arg0)
+            {
+            case 0:SetMonData(pp,MON_DATA_HP_IV, &iv);break;
+            case 1:SetMonData(pp,MON_DATA_ATK_IV, &iv);break;
+            case 2:SetMonData(pp,MON_DATA_DEF_IV, &iv);break;
+            case 3:SetMonData(pp,MON_DATA_SPATK_IV, &iv);break;
+            case 4:SetMonData(pp,MON_DATA_SPDEF_IV, &iv);break;
+            case 5:SetMonData(pp,MON_DATA_SPEED_IV, &iv);break;
+            case 6:SetMonData(pp,MON_DATA_HP_IV, &iv);
+            SetMonData(pp,MON_DATA_ATK_IV, &iv);
+            SetMonData(pp,MON_DATA_DEF_IV, &iv);
+            SetMonData(pp,MON_DATA_SPATK_IV, &iv);
+            SetMonData(pp,MON_DATA_SPDEF_IV, &iv);
+            SetMonData(pp,MON_DATA_SPEED_IV, &iv);
+            break;
+            default:break;
+            }
+            RecalcPartyPokemonStats(pp);
+#endif
+            break;
     }
 
     return FALSE;
